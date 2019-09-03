c3ce8a9bfe15c5faf70adf4acdde9e18cd483a36
Jean Traullé <jtraulle@opencomp.fr>
2019-09-03 16:05:45 +0200

    Keep warnings log to see how to improve

--- generateUnifiedIndex.sh
+++ generateUnifiedIndex.sh
@@ -8,7 +8,11 @@ cp template_index/dolibarr.png build_index/
 cp template_index/zip.png build_index/
 cp template_index/pdf.png build_index/
 cp template_index/web.png build_index/
+cp template_index/log.png build_index/
+cp template_index/lua.png build_index/
+cp template_index/tools.png build_index/
 cat template_index/index-header.html > build_index/index.html
+cat template_index/index-header.html > build_index/logs.html
 
 while read version; do
   echo "<li><strong style='font-size: large;'>→ $version</strong></li><ul>" >> build_index/index.html
@@ -16,7 +20,13 @@ while read version; do
   echo "<li><a href="https://gitlab.com/jtraulle/dolibarr-doxygen/-/archive/gh-pages/dolibarr-doxygen-gh-pages.zip?path=$version"><img style='vertical-align: middle;' src="zip.png" /> download .zip offline HTML version</a></li>" >> build_index/index.html
   echo "<li><a href="$version/dolibarr-$version.pdf"><img style='vertical-align: middle;' src="pdf.png" /> PDF version for printing or offline usage</a></li>" >> build_index/index.html
   echo "</ul>" >> build_index/index.html
+
+  echo "<li><strong style='font-size: large;'>→ $version</strong></li><ul>" >> build_index/logs.html
+  echo "<li><a href="$version/doxygen-warnings.log"><img style='vertical-align: middle;' src="log.png" /> Doxygen warnings</a></li>" >> build_index/logs.html
+  echo "<li><a href="$version/lualatex-warnings.log"><img style='vertical-align: middle;' src="lua.png" /> LuaLaTeX warnings</a></li>" >> build_index/logs.html
+  echo "</ul>" >> build_index/logs.html
 done <versions/dolibarrVersions
 
 cat template_index/index-footer.html >> build_index/index.html
+cat template_index/index-footer.html >> build_index/logs.html
 
