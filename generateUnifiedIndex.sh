046e3272ca06becb7201ec3b7983cf5d69cd9af2
Jean Traullé <jtraulle@opencomp.fr>
2019-09-03 21:59:55 +0200

    Adding icons to version list ; Fixing some XHTML validation errors

--- generateUnifiedIndex.sh
+++ generateUnifiedIndex.sh
@@ -11,20 +11,30 @@ cp template_index/web.png build_index/
 cp template_index/log.png build_index/
 cp template_index/lua.png build_index/
 cp template_index/tools.png build_index/
+cp template_index/git.png build_index/
+cp template_index/tag.png build_index/
 cat template_index/index-header.html > build_index/index.html
 cat template_index/index-header.html > build_index/logs.html
 
 while read version; do
-  echo "<li><strong style='font-size: large;'>→ $version</strong></li><ul>" >> build_index/index.html
-  echo "<li><a href="$version/index.html"><img style='vertical-align: middle;' src="web.png" /><strong> browse HTML version (online)</strong></a></li>" >> build_index/index.html
-  echo "<li><a href="https://gitlab.com/jtraulle/dolibarr-doxygen/-/archive/gh-pages/dolibarr-doxygen-gh-pages.zip?path=$version"><img style='vertical-align: middle;' src="zip.png" /> download HTML version .zip archive (for offline usage)</a></li>" >> build_index/index.html
-  echo "<li><a href="$version/dolibarr-$version.pdf"><img style='vertical-align: middle;' src="pdf.png" /> download PDF version (for printing or offline usage)</a></li>" >> build_index/index.html
-  echo "</ul>" >> build_index/index.html
 
-  echo "<li><strong style='font-size: large;'>→ $version</strong></li><ul>" >> build_index/logs.html
-  echo "<li><a href="$version/doxygen-warnings.log"><img style='vertical-align: middle;' src="log.png" /> Doxygen warnings</a></li>" >> build_index/logs.html
-  echo "<li><a href="$version/lualatex-warnings.log"><img style='vertical-align: middle;' src="lua.png" /> LuaLaTeX warnings</a></li>" >> build_index/logs.html
-  echo "</ul>" >> build_index/logs.html
+  if [ $version = "develop" ]
+  then
+     icon="<img style='position:relative; top:6px;' src='git.png' alt='Git icon' />"
+  else
+     icon="<img style='position:relative; top:3px;' src='tag.png' alt='Tag icon' />"
+  fi
+
+  echo "<li><strong style='font-size: large;'>$icon $version</strong><ul style='padding-bottom: 40px;'>" >> build_index/index.html
+  echo "<li><a href='$version/index.html'><img style='vertical-align: middle;' alt='Web globe icon' src='web.png' /><strong> browse HTML version (online)</strong></a></li>" >> build_index/index.html
+  echo "<li><a href='https://gitlab.com/jtraulle/dolibarr-doxygen/-/archive/gh-pages/dolibarr-doxygen-gh-pages.zip?path=$version'><img style='vertical-align: middle;' alt='Archive file icon' src='zip.png' /> download HTML version .zip archive (for offline usage)</a></li>" >> build_index/index.html
+  echo "<li><a href='$version/dolibarr-$version.pdf'><img style='vertical-align: middle;' alt='PDF file icon' src='pdf.png' /> download PDF version (for printing or offline usage)</a></li>" >> build_index/index.html
+  echo "</ul></li>" >> build_index/index.html
+
+  echo "<li><strong style='font-size: large;'>$icon $version</strong><ul>" >> build_index/logs.html
+  echo "<li><a href='$version/doxygen-warnings.log'><img style='vertical-align: middle;' alt='Log event icon' src='log.png' /> Doxygen warnings</a></li>" >> build_index/logs.html
+  echo "<li><a href='$version/lualatex-warnings.log'><img style='vertical-align: middle;' alt='Lua LaTeX icon' src='lua.png' /> LuaLaTeX warnings</a></li>" >> build_index/logs.html
+  echo "</ul></li>" >> build_index/logs.html
 done <versions/dolibarrVersions
 
 cat template_index/index-footer.html >> build_index/index.html
